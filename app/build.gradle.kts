4c2a3d0d017f878f2ef72b717f9e0f8908ef2ff5
Amatory6644 <t.l1999@yandex.ru>
2026-02-09 12:49:49 +0300

    Upgrade AGP, lifecycle, actCompose, composeBom, compileSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,16 +2,22 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("org.jetbrains.kotlin.kapt") version "2.3.10"
+   // id("com.google.devtools.ksp") version "2.2.21-2.0.5"
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("androidx.room") version "2.8.4" apply false
+
+
 }
 
 android {
     namespace = "com.example.testwork"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.testwork"
         minSdk = 33
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -31,16 +37,31 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    kotlin.compilerOptions{
+        jvmTarget
     }
+//    kotlinOptions {
+//        jvmTarget = "1.8"
+//    }
     buildFeatures {
+        //noinspection DataBindingWithoutKapt
+        dataBinding = true
         compose = true
+    }
+    packaging{
+        //resources.pickFirsts
+//        resources.excludes.add("com.google.dagger:dagger-compiler:2.57.1/dagger-compiler-2.57.1.jar")
+        // it helped delete
     }
 }
 
 dependencies {
 
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.material)
+    implementation(libs.hilt.android)
+//implementation(libs.hilt.android.compiler)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
